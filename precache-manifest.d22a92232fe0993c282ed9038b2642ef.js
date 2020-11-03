131dba0ce20e464c96ef75b68be844cc90685928
Nikki Maree <61126225+NMaree@users.noreply.github.com>
2020-11-04 11:29:20 +1300

    Add files via upload

---
+++ precache-manifest.d22a92232fe0993c282ed9038b2642ef.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "09bb62e1b53aa49bd918488e1149c7ed",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f2ac537add1019d00334",
+    "url": "/static/css/main.316d1aa5.chunk.css"
+  },
+  {
+    "revision": "e72f38279c031e6b1313",
+    "url": "/static/js/2.3e8cc54b.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/static/js/2.3e8cc54b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f2ac537add1019d00334",
+    "url": "/static/js/main.fdeb5b01.chunk.js"
+  },
+  {
+    "revision": "9b4e396c183e42c1fa5c",
+    "url": "/static/js/runtime-main.09b85ec0.js"
+  },
+  {
+    "revision": "aaa0b5aebf2f0e027a1e0da8d1a9cc93",
+    "url": "/static/media/logo5.aaa0b5ae.png"
+  }
+]);
